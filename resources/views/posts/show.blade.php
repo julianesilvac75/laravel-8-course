feebcd8629ca9a5a528a717006b645c74f4d93bb
Juliane <julianesilvac75@gmail.com>
2022-09-06 16:24:47 +0100

    class 30: conditional rendering

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,13 @@
 @section('title', $post['title'])
 
 @section('content')
+
+@if ($post['is_new'])
+    <div>a new post, using if</div>
+@else
+    <div>post is old, using elseif/else</div>
+@endif
+
     <h1>{{ $post['title'] }}</h1>
     <p>{{ $post['content'] }}</p>
 @endsection
