2171835c20ee25a35d96ec82bf80b68d12633714
Juliane <julianesilvac75@gmail.com>
2022-09-06 16:27:57 +0100

    class 31: conditional rendering alternatives

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,6 +10,14 @@
     <div>post is old, using elseif/else</div>
 @endif
 
+@unless ($post['is_new'])
+    <div>it's old, using unless</div>
+@endunless
+
     <h1>{{ $post['title'] }}</h1>
     <p>{{ $post['content'] }}</p>
+
+@isset($post['has_comments'])
+    <div>the post has comments, using isset</div>
+@endisset
 @endsection
