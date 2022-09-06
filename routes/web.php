7823f67edcaaa44b98d1c389b0c82db643cd37a4
Juliane <julianesilvac75@gmail.com>
2022-09-06 16:06:49 +0100

    class 26: understanding templating, views and blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return'home page';
+    return view('home.index', []);
 })->name('home.index');
 
 Route::get('/contact', function () {
