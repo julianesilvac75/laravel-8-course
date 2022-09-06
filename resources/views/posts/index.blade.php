8d9e42889f10a04902533dcb7897753e3a561dff
Juliane <julianesilvac75@gmail.com>
2022-09-06 16:41:50 +0100

    class 33: more control inside loops

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,8 +4,17 @@
 
 @section('content')
 
-    @foreach ($posts as $key => $post)
-        <div>{{ $key }}. {{ $post['title'] }}</div>
-    @endforeach
+        @forelse ($posts as $key => $post)
+
+            {{-- @break($key === 2) --}}
+            {{-- @continue($key === 1) --}}
+            @if ($loop->even)
+                <div>{{ $key }}. {{ $post['title'] }}</div>
+            @else
+                <div style="background-color: silver">{{ $key }}. {{ $post['title'] }}</div>
+            @endif
+        @empty
+            <div>No posts found</div>
+        @endforelse
 
 @endsection
