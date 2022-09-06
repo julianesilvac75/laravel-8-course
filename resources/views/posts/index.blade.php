67ac40614b8dceb6af96d382e2e23fe007fdb9ea
Juliane <julianesilvac75@gmail.com>
2022-09-06 16:33:29 +0100

    class 32: loops in templates

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Blog Posts')
+
+@section('content')
+
+    @foreach ($posts as $key => $post)
+        <div>{{ $key }}. {{ $post['title'] }}</div>
+    @endforeach
+
+@endsection
