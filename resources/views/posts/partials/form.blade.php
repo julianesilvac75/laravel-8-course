16fc4a7ca5cf86759d7ad4ffd78a33e14b854d1a
Juliane <julianesilvac75@gmail.com>
2022-09-09 13:02:13 +0100

    class 80: styling forms

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,12 +1,14 @@
-<div>
-    <input type="text" name="title" value="{{ old('title', optional($post ?? null)->title) }}">
+<div class="form-group">
+    <label for="title">Title</label>
+    <input class="form-control" id="title" type="text" name="title" value="{{ old('title', optional($post ?? null)->title) }}">
 </div>
 @error('title')
     <div>{{ $message }}</div>
 @enderror
 
-<div>
-    <textarea name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
+<div class="form-group">
+    <label for="content">Content</label>
+    <textarea class="form-control" id="content" name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
 </div>
 
 @if ($errors->any())
