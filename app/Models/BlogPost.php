bc0a9b6ff718f9c8516801ff316f9a3b6a5d61c2
Juliane <julianesilvac75@gmail.com>
2022-09-12 10:58:25 +0100

    class 95: one to many relation with migration

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model
 {
     protected $fillable = ['title', 'content'];
-    use HasFactory;
+
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
 }
