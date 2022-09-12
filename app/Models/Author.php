8811ba69eea86f372f6591005444c79bf1a576c4
Juliane <julianesilvac75@gmail.com>
2022-09-12 12:46:10 +0100

    class 105: model factory callbacks (afterCreating, afterMaking)

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
+    use HasFactory;
+    
     public function profile()
     {
         return $this->hasOne('App\Models\Profile');
--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    use HasFactory;
+    
     public function author()
     {
         return $this->belongsTo('App\Models\Author');
