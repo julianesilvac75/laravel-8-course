b4036675e91c06f97fca4eebde0843cdb3e93de0
Juliane <julianesilvac75@gmail.com>
2022-09-12 11:24:00 +0100

    class 98: lazy loading vs eager loading

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
@@ -15,6 +16,18 @@ class PostsController extends Controller
      */
     public function index()
     {
+        DB::enableQueryLog();
+
+        $posts = BlogPost::with('comments')->get();
+
+        foreach ($posts as $post) {
+            foreach ($post->comments as $comment) {
+                echo $comment->content;
+            }
+        }
+
+        dd(DB::getQueryLog());
+
         return view('posts.index', ['posts' => BlogPost::all()]);
     }
 
