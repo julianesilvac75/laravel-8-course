01d3008889e6cc300d1932328055f25f00958fab
Juliane <julianesilvac75@gmail.com>
2022-09-13 12:50:22 +0100

    class 124: individual seeder classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\BlogPost;
-use App\Models\Comment;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,19 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $doe = User::factory()->newJaneDoe()->create();
-        $else = User::factory(20)->create();
-
-        $users = $else->concat([$doe]);
-
-        $posts = BlogPost::factory(50)->make()->each(function ($post) use ($users) {
-            $post->user_id = $users->random()->id;
-            $post->save();
-        });
-
-        $comments = Comment::factory(150)->make()->each(function ($comment) use ($posts) {
-            $comment->blog_post_id = $posts->random()->id;
-            $comment->save();
-        });
+        $this->call([
+            UsersTableSeeder::class,
+            BlogPostsTableSeeder::class,
+            CommentsTableSeeder::class
+        ]);
     }
 }
