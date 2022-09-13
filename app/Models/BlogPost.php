f7ab39a5f66bf2919db4af40554eff2186db7de4
Juliane <julianesilvac75@gmail.com>
2022-09-13 14:48:10 +0100

    class 132: authorization introduction

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -19,6 +19,11 @@ class BlogPost extends Model
         return $this->hasMany('App\Models\Comment');
     }
 
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
     // delete event for tables with foreign keys
     static function boot()
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -31,6 +30,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function blogPosts()
+    {
+        return $this->hasMany('App\Models\BlogPost');
+    }
 
     /**
      * The attributes that should be cast to native types.
