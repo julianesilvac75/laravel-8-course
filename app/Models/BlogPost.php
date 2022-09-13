f3d48644455c6790b88d2a75f4c1f7df0dc993ef
Juliane <julianesilvac75@gmail.com>
2022-09-13 14:10:41 +0100

    class 126: deleting related model using model events

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -15,4 +15,14 @@ class BlogPost extends Model
     {
         return $this->hasMany('App\Models\Comment');
     }
+
+    // delete event for tables with foreign keys
+    static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+    }
 }
