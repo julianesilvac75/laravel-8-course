82faf128258de5d28cc3dc76503479d4f4fb110d
Juliane <julianesilvac75@gmail.com>
2022-09-14 13:05:55 +0100

    class 148: practical: local query scope - most active users last month

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -53,6 +53,25 @@
                         </ul>
                     </div>
                 </div>
+
+                {{-- Most active authors last month --}}
+                <div class="row mt-4">
+                    <div class="card" style="width: 100%;">
+                        <div class="card-body">
+                            <h5 class="card-title">Most Active Last Month</h5>
+                            <h6 class="card-subtitle mb-2 test-muted">
+                                Users with most posts writen in the last month
+                            </h6>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            @foreach ($mostActiveLastMonth as $user)
+                                <li class="list-group-item">
+                                    {{ $user->name }}
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
