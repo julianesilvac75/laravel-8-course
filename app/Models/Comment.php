e98cc3f4b0e015a479ab61d94309308cc9976c5b
Juliane <julianesilvac75@gmail.com>
2022-09-14 12:14:05 +0100

    class 144: global query scopes and potential issues

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,5 +16,13 @@ class Comment extends Model
     public function blogPost()
     {
         return $this->belongsTo('App\Models\BlogPost');
+    }
+
+    static function boot()
+    {
+        parent::boot();
+        
+        //global scope to order comments by the newest
+        static::addGlobalScope(new LatestScope);
     }
 }
