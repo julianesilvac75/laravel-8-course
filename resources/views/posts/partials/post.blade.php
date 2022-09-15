eab1de9f107c26ea31962293df1b2500b3cb485d
Juliane <julianesilvac75@gmail.com>
2022-09-15 10:30:31 +0100

    class 157: laravel debugbar

--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -23,20 +23,25 @@
     @endif
     
     <div class="mb-3">
-        @can('update', $post)
-            <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
-        @endcan
-        
-        @if (!$post->trashed())
-            @can('delete', $post)
-                <form class="d-inline" action="{{ route('posts.destroy', ['post' =>$post->id]) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-        
-                    <input class="btn btn-primary" type="submit" value="Delete">
-                </form>
+        {{-- Apparently, @auth is not necessary anymore on Laravel 8  --}}
+        {{-- @auth --}}
+            @can('update', $post)
+                <a class="btn btn-primary" href="{{ route('posts.edit', ['post' => $post->id]) }}">Edit</a>
             @endcan
-        @endif
+        {{-- @endauth --}}
+        
+        {{-- @auth --}}
+            @if (!$post->trashed())
+                @can('delete', $post)
+                    <form class="d-inline" action="{{ route('posts.destroy', ['post' =>$post->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+            
+                        <input class="btn btn-primary" type="submit" value="Delete">
+                    </form>
+                @endcan
+            @endif
+        {{-- @endauth --}}
 
     </div>
 </div>
