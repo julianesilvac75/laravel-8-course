27b66987aa88ac50707467d5dd9d6ac8fdda509c
Juliane <julianesilvac75@gmail.com>
2022-09-16 15:08:01 +0100

    class 182: file upload form

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,14 +1,21 @@
-<div class="form-group">
-    <label for="title">Title</label>
-    <input class="form-control" id="title" type="text" name="title" value="{{ old('title', optional($post ?? null)->title) }}">
-</div>
-@error('title')
-    <div class="alert alert-danger">{{ $message }}</div>
-@enderror
+<div>
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input class="form-control" id="title" type="text" name="title" value="{{ old('title', optional($post ?? null)->title) }}">
+    </div>
+    @error('title')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    
+    <div class="form-group">
+        <label for="content">Content</label>
+        <textarea class="form-control" id="content" name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
+    </div>
 
-<div class="form-group">
-    <label for="content">Content</label>
-    <textarea class="form-control" id="content" name="content">{{ old('content', optional($post ?? null)->content) }}</textarea>
-</div>
+    <div class="form-group">
+        <label for="thumbnail">Thumbnail</label>
+        <input class="form-control-file" id="thumbnail" type="file" name="thumbnail">
+    </div>
 
-<x-errors />
+    <x-errors />
+</div>
