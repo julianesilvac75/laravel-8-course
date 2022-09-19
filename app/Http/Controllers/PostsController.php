4d5283a781b590d46b5f15c2544dda9cad7e64de
Juliane <julianesilvac75@gmail.com>
2022-09-19 11:55:21 +0100

    class 197: OneToOne polymorphic associating

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -56,7 +56,7 @@ class PostsController extends Controller
         if ($request->hasFile('thumbnail')) {
             $path = $request->file('thumbnail')->store('thumbnails');
             $post->image()->save(
-                Image::create(['path' => $path])
+                Image::make(['path' => $path])
             );
         }
 
@@ -160,7 +160,7 @@ class PostsController extends Controller
                 $post->image->save();
             } else {
                 $post->image()->save(
-                    Image::create(['path' => $path])
+                    Image::make(['path' => $path])
                 );
             }
         }
