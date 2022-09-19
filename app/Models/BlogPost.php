49ffec68c275681b459943069824acd62410daa6
Juliane <julianesilvac75@gmail.com>
2022-09-19 13:57:13 +0100

    class 207: ManyToMany polymorphic relations

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -29,7 +29,7 @@ class BlogPost extends Model
 
     public function tags()
     {
-        return $this->belongsToMany('App\Models\Tag')->withTimestamps();
+        return $this->morphToMany('App\Models\Tag', 'taggable')->withTimestamps();
     }
 
     public function image()
