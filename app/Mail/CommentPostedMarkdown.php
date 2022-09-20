890a8a102e9c3404673924883b5a0e1478ea2972
Juliane <julianesilvac75@gmail.com>
2022-09-20 11:40:03 +0100

    class 221: optional email queuing and execution delay

--- app/Mail/CommentPostedMarkdown.php
+++ app/Mail/CommentPostedMarkdown.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class CommentPostedMarkdown extends Mailable implements ShouldQueue
+class CommentPostedMarkdown extends Mailable
 {
     use Queueable, SerializesModels;
 
