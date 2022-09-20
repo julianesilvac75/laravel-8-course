51d01263d5a6a4d738f2564f7b23ef1911f89e94
Juliane <julianesilvac75@gmail.com>
2022-09-20 10:47:06 +0100

    class 216: creating the markdown mailable class

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreComment;
-use App\Mail\CommentPosted;
+use App\Mail\CommentPostedMarkdown;
 use App\Models\BlogPost;
 use Illuminate\Support\Facades\Mail;
 
@@ -22,7 +22,7 @@ class PostCommentController extends Controller
         ]);
 
         Mail::to($post->user)->send(
-            new CommentPosted($comment)
+            new CommentPostedMarkdown()
         );
 
         return redirect()
