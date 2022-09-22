7e8a684ef854d18af3ed0daee1ec5e8d8b51cfb6
Juliane <julianesilvac75@gmail.com>
2022-09-22 14:17:40 +0100

    class 247: postman - a quite long introduction

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/single', AboutController::class);
 
 Route::resource('posts', PostsController::class);
 Route::get('/posts/tag/{tag}', [PostTagController::class, 'index'])->name('posts.tags.index');
-Route::resource('posts.comments', PostCommentController::class)->only(['store']);
+Route::resource('posts.comments', PostCommentController::class)->only(['index', 'store']);
 
 Route::resource('users', UserController::class)->only(['show', 'edit', 'update']);
 Route::resource('users.comments', UserCommentController::class)->only(['store']);
