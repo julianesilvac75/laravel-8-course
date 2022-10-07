4d269d04edbbf2538fbb803c7b2426484261dd72
Juliane <julianesilvac75@gmail.com>
2022-10-07 12:59:47 +0100

    class 264: API authorization

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 // use Throwable;
 
@@ -37,6 +38,10 @@ class Handler extends ExceptionHandler
     {
         $this->renderable(function (NotFoundHttpException $e, $request) {
             return Route::respondWithRoute('api.fallback');
+        });
+
+        $this->renderable(function (AccessDeniedHttpException $e, $request) {
+            return response()->json(['message' => $e->getMessage()], 403);
         });
     }
 }
